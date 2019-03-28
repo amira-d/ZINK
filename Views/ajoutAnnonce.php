017e40d9c9596af54006492a097eff154cebba61
amira-d <doghriamira@gmail.com>
2019-03-28 12:08:07 +0100

    nn

--- Views/ajoutAnnonce.php
+++ Views/ajoutAnnonce.php
@@ -6,10 +6,17 @@ $msg="";
 
 if ( isset($_POST['submit']) and isset($_POST['titre']) and isset($_POST['type']) and isset($_POST['description']) )
 {
-	if (!empty($_POST['submit']) and !empty($_POST['titre']) and !empty($_POST['description']) and !empty($_POST['type']))
+	if (empty($_POST['submit']) and empty($_POST['titre']) and empty($_POST['description']) and empty($_POST['type']))
      {	
-			$target= "ZINK/" .basename($_FILES['image']['titre']);
-			$image = $_FILES['image']['titre'];
+     	echo "<script>alert(\"Veuillez vérifier les champs ! Tous les champs doivent être remplis . \")</script>";
+	header("Refresh: 2 ;url=annonces-produit.html");
+			
+     }
+
+     else
+     {
+	        $target= "ZINK/" .basename($_FILES['image']['name']);
+			$image = $_FILES['image']['name'];
 
 					if (move_uploaded_file($_FILES['image']['tmp_name'], $target ))
 						{
@@ -26,15 +33,7 @@ if ( isset($_POST['submit']) and isset($_POST['titre']) and isset($_POST['type']
 	        header("Refresh: 2 ;url=annonces-produit.html");
 			echo "<script>alert(\"Annonce enregistrée avec succès\")</script>";
 		    header("Refresh: 2 ;url=annonces-produit.html");
-	
-     }
-
-     else
-     {
-	echo "<script>alert(\"Veuillez vérifier les champs ! Tous les champs doivent être remplis . \")</script>";
-	header("Refresh: 2 ;url=annonces-produit.html");
 	 }
-
 }
 else
 {
