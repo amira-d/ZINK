236fec2dfdf8eeafd318108919909014ffbcbfec
amira-d <doghriamira@gmail.com>
2019-04-21 11:29:22 +0100

    hello

--- Views/ZINK_FI/jobs.php
+++ Views/ZINK_FI/jobs.php
@@ -66,65 +66,18 @@
 
   <!-- Header
    ================================================== -->
-   <header id="home" style="height:100px;">
-
-       <!-- Start cSlider -->
-            <div id="da-slider" class="da-slider">
-                <div class="triangle"></div>
-                <!-- mask elemet use for masking background image -->
-                <div class="mask"></div>
-                <!-- All slides centred in container element -->
-                <div class="container">
-                    <!-- Start first slide -->
-                    <div class="da-slide">
-                        <h2 class="fittext2">Welcome to pluton theme</h2>
-                        <h4>Clean & responsive</h4>
-                        <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.</p>
-                        <a href="#" class="da-link button">Read more</a>
-                        <div class="da-img">
-                            <img src="../web/coffee.jpg" alt="image01" width="400">
-                        </div>
-                    </div>
-                    <!-- End first slide -->
-                    <!-- Start second slide -->
-                    <div class="da-slide">
-                        <h2>Easy management</h2>
-                        <h4>Easy to use</h4>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                        <a href="#" class="da-link button">Read more</a>
-                        <div class="da-img">
-                            <img src="../web/console.jpg" width="320" alt="image02">
-                        </div>
-                    </div>
-                    <!-- End second slide -->
-                    <!-- Start third slide -->
-                    <div class="da-slide">
-                        <h2>Revolution</h2>
-                        <h4>Customizable</h4>
-                        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-                        <a href="#" class="da-link button">Read more</a>
-                        <div class="da-img">
-                            <img src="images/Slider03.png" width="320" alt="image03">
-                        </div>
-                    </div>
-                    <!-- Start third slide -->  
-                    <!-- Start cSlide navigation arrows -->
-                    <div class="da-arrows">
-                        <span class="da-arrows-prev"></span>
-                        <span class="da-arrows-next"></span>
-                    </div>
-                    <!-- End cSlide navigation arrows -->
-                        <nav id="nav-wrap">
+   <header id="testimonials" style="height:100px;">
+       <nav id="nav-wrap">
 
          <a class="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
         <a class="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
-
+      
          <ul id="nav" class="nav">
             <li><a class="smoothscroll" href="#home">Home</a></li>
             <li><a class="smoothscroll" href="#about">About</a></li>
             <li><a class="smoothscroll" href="#portfolio">Works</a></li>
             <li><a class="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li class="current"><a class="smoothscroll" href="#news">Z-News</a></li>
+            <li class="current"><a class="smoothscroll" href="#news">Z-News & Jobs</a></li>
             <li><a class="smoothscroll" href="#contact">Contact</a></li>
          </ul> <!-- end #nav -->
 
@@ -141,43 +94,9 @@
 
  
 
-<section id="news" style="height: 3000px; width: 1800px;">
-<div class="menu-upload-form" >
-
-<?PHP
-
-include "C:/wamp64/www/ZINK/zink/Cores/annonceC.php";
-$ann1C=new AnnonceC();
-$listeannonces=$ann1C->afficherAnnonce();
-
-//var_dump($listeannonces->fetchAll());
 
 
-while ($data=$listeannonces->fetch())
-{
-     {echo "<div class='col-md-4 col-sm-6 graphics ads' style='width:340px; margin: 64px;'>";
-     echo "<div style='width:320px ; height:320px;'>";
-                           echo "<img src=' ../web/".$data['image']."' alt='image' class='img-responsive'/>";
-                           echo"</div>";
-                            echo "<div style='width:200px ; height:100px;'>";
-                           echo "<p>".$data['titre']."</p>";
-                            echo "<p>".$data['description']."</p>";
-                            echo "<div class='portfolio_item_hover'>";
-                              echo  "<div class='portfolio-border clearfix'>";
-                                    echo "<div class='item_info'>";
-                                       echo "<p>".$data['type']." </p>";
-                                    echo"</div>";
-                                echo "</div>";
-                            echo "</div>";
-                            echo"</div>";
-                    echo"</div>";}
-}
-?>
-
-</div>
-</section>
-
-<section id="contact" >
+<section id="contact">
 
          <div class="row section-head" style="margin-left: 60px;">
 
@@ -196,7 +115,7 @@ while ($data=$listeannonces->fetch())
 
          </div>
 
-         <div class="row" style="margin-left: 60px;">
+         <div class="row" style="margin-left: 60px;"> 
 
             <div class="eight columns">
 
@@ -263,13 +182,13 @@ while ($data=$listeannonces->fetch())
 
    </section> <!-- Contact Section End-->
 
-   <footer style="margin-left: 80px;">
+   <footer>
 
-      <div class="row" style="margin-left: 60px;">
+      <div class="row">
 
          <div class="twelve columns">
 
-            <ul class="social-links" >
+            <ul class="social-links">
                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
