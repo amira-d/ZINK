89afe12917f0c16a48c9b0e116b5839e1486dc52
amira-d <doghriamira@gmail.com>
2019-04-23 21:27:23 +0100

    f

--- Views/supprimerAnnonce.php
+++ Views/supprimerAnnonce.php
@@ -1,8 +1,8 @@
 <?PHP
 include "../Cores/annonceC.php";
 $annonce=new annonceC();
-if (isset($_POST["id"])){
-	$annonceC->supprimerannonce($_POST["id"]);
+if (isset($_POST["titre"])){
+	$annonce->supprimerannonce($_POST["titre"]);
 	header('Location: annonces-produit.php');
 }
 
